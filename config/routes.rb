fa8a15872bc47cb138ff13a3a0fa14be5678423a
Mathieu Garin <mathieugarin08@gmail.com>
2022-11-16 09:07:23 +0100

    HELLO

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'articles', to: 'articles#index', as: :articles
+  get 'articles/new', to: 'articles#new', as: :new
+  get 'articles/:id', to: 'articles#show', as: :article
+  post 'articles', to: 'articles#create'
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
 end
